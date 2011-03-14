93a4c7a2c206882c846fdfcf6645cb8df37a0825
Makoto Inoue <inouemak@googlemail.com>
2011-03-14 13:33:58 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bubblyapp::Application.config.secret_token = 'c0cec56a75c6ab915a7239e5eafe8131914fbac77f29fe1ab1c5351edaba36f0447fdef74574ac6bea053875d8ecd9126b3218db3cadfd310cd6334d2ba15804'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bubblyapp::Application.config.session_store :cookie_store, :key => '_bubblyapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bubblyapp::Application.config.session_store :active_record_store
