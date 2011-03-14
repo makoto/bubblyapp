25e755ef92a495ec079a852348da7f693573d3fb
Makoto Inoue <inouemak@googlemail.com>
2011-03-14 23:18:16 +0000

    Added tweet summary

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,9 @@
 class Tweet < ActiveRecord::Base
   has_many :mentions
   validates :id_str, :uniqueness => true
+
+  def self.summary
+    group('concat(YEAR(tweeted_at), "/",   MONTH(tweeted_at), "/", DAY(tweeted_at), " ", HOUR(tweeted_at) , ":", MINUTE(tweeted_at))').count
+  end
+
 end
