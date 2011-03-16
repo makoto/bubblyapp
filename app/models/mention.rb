e069e5223c5a399d4d51560cd87187bb4e9314b6
Makoto Inoue <inouemak@googlemail.com>
2011-03-16 22:13:32 +0000

    Extracted some logic to class method

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,4 +1,19 @@
 class Mention < ActiveRecord::Base
   belongs_to :tweet
   belongs_to :user
+  
+  def self.summary(time)
+    mentions = self.joins(:tweet).where("tweets.tweeted_at > ? and twitter not in  ('@sxsw', '@foursquare')", time).group(:twitter).order("count(*) desc").limit(100)
+    speakers = Speaker.includes(:schedule).where("twitter in (?)", mentions.map{|m| m.twitter})
+
+    #Need to massively refactor
+    @mentions_summary = mentions.count.reduce({}) do |a, b| 
+      name = b[0] 
+      schedule = if speaker = speakers.select{|s| s.twitter == name}[0]
+       speaker.schedule
+      end
+      a[name] = {:count => b[1], :speaker => schedule}
+      a
+    end
+  end
 end
