d8231b4f85a6f10607e3b5bf62001c2413896919
Makoto Inoue <inouemak@googlemail.com>
2011-03-16 15:09:26 +0000

    Fixed routing problem

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Bubblyapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "tweets"
+  root :to => "tweets#index"
 
   # See how all your routes lay out with "rake routes"
 
